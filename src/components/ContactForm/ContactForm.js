4bf8e4cbc86a354178e1f51058301240fb868831
Oksana Komarnytska <okazimiriv@gmail.com>
2023-07-10 21:26:43 +0200

    validat

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -29,7 +29,7 @@ const ContactForm = () => {
   const handleSubmit = event => {
     event.preventDefault();
 
-    if (name.includes(Number(number))) {
+    if (name.includes(Number(name))) {
       return toast.warn('Name may contain only letters', {
         position: toast.POSITION.TOP_CENTER,
         icon: false,
@@ -81,18 +81,19 @@ const ContactForm = () => {
           name="name"
           value={name}
           placeholder="Harry Potter"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
           onChange={handleChange}
         />
         <Label>Number</Label>
         <Input
-          type="tel"
+          type="phone"
           name="number"
           value={number}
           placeholder="123456"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          minlength="6"
+          // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
           onChange={handleChange}
